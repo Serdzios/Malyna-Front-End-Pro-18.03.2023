f8651a3c4dc12f72ed5c9a3753aa249472adfef9
serdz <serdzio.malina@gmail.com>
2023-06-13 15:49:02 +0300

    adding homework 39

--- New Project/photos-app-main/js/main.js
+++ New Project/photos-app-main/js/main.js
@@ -45,6 +45,19 @@ function generateMockData() {
 
   return photos;
 }
-
+//Відображення фотографій користувача
 const mockData = generateMockData();
-console.log(mockData);
+
+const picturesBlock = document.querySelector(".pictures");
+const pictureTemplate = document.querySelector("#picture");
+
+for (let el of mockData) {
+  const picture = pictureTemplate.content.cloneNode(true);
+  const image = picture.querySelector(".picture__img");
+  const comments = picture.querySelector(".picture__comments");
+  const likes = picture.querySelector(".picture__likes");
+  image.src = `${el.url}`;
+  comments.textContent = `${el.comments.length}`;
+  likes.textContent = `${el.likes}`;
+  picturesBlock.append(picture);
+}
